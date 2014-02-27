42e45ae0309e09c388c6255d9b6270a04e5507f7
guillaume <debaisieux.guillaume@gmail.com>
2014-02-27 19:29:54 +0100

    modification formulaire connexion

--- hooter/hooter_app/forms.py
+++ hooter/hooter_app/forms.py
@@ -4,8 +4,11 @@ from django import forms
 from hooter_app.models import Utilisateur
 
 class ConnexionForm(forms.ModelForm):
+	
+	email = forms.EmailField(required=True,widget=forms.EmailInput(attrs={'placeholder': 'Email'}),label='')
+	mot_passe = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}),label='')
 	class Meta:
 		model = Utilisateur
-		#fields = ['email','mot_passe']
-		email = forms.EmailField()
-		mot_passe = forms.PasswordInput()
+		fields = ['email','mot_passe']
+		
+		
--- hooter/hooter_app/views.py
+++ hooter/hooter_app/views.py
@@ -1,16 +1,37 @@
-
+from django.shortcuts import redirect
 from hooter_app.forms import ConnexionForm
+from hooter_app.models import Utilisateur
 from django.shortcuts import render,get_object_or_404
-
+from django.views.decorators.csrf import csrf_protect
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from hooter_app.models import Utilisateur
 # Create your views here.
 
 def index(request):	
-	formulaire = ConnexionForm(request.POST)
+	formulaire = ConnexionForm()
 	contexte = {'formulaire':formulaire,}
 	return render(request, 'index.html',contexte)
 
+@csrf_protect
+def connexion(request):
+	if 'email' in request.POST:
+		formulaire=ConnexionForm(request.POST)
+		contexte = {'formulaire':formulaire,}
+		if formulaire.is_valid():
+			try:
+				utilisateur = Utilisateur.objects.get(email=formulaire.cleaned_data['email'],mot_passe=formulaire.cleaned_data['mot_passe'])
+				return redirect('profile_view',utilisateur.pseudo)
+			except Utilisateur.DoesNotExist:
+				contexte['errors']='Utilisateur inconnu'
+				return render(request, 'index.html',contexte)
+		else:
+			
+			return render(request, 'index.html',contexte)
+	else:
+		return redirect('index')
+		
+	
 
 
 def profile_view(request, pseudo):
