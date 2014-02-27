8eb6870d6b911131d272be2cf7b17e5d69eede67
amonnier <monnier-amelie@hotmail.fr>
2014-02-27 17:33:52 +0100

    ajout profile_view, recuperation infos profil

--- hooter/hooter_app/urls.py
+++ hooter/hooter_app/urls.py
@@ -4,5 +4,6 @@ from hooter_app import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
+	url(r'^(?P<pseudo>\w+)/$',views.profile_view, name='profile_view'),
 
 )
--- hooter/hooter_app/views.py
+++ hooter/hooter_app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
+from django.http import HttpResponse
+from hooter_app.models import Utilisateur
 # Create your views here.
 
 def index(request):
 	return render(request, 'index.html')
+
+
+def profile_view(request, pseudo):
+	u = Utilisateur.objects.get(pseudo=pseudo)
+	infos=get_object_or_404(Utilisateur, pseudo=u)
+	contexte={'infos' : infos}
+	return render(request, 'profile_view.html',contexte)
